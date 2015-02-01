950377c2f601e35bdb684b1eee5bb9fc37d95e4c
JohnnyHu9 <r17d09@gmail.com>
2015-01-31 22:51:22 -0500

    add projects

--- ZhixiangHu_Portfolio_Site/js/app.js
+++ ZhixiangHu_Portfolio_Site/js/app.js
@@ -3,3 +3,9 @@
 //  Author's name : Zhixiang Hu
 //  Web site name : Zhixiang Hu Protfolio Site
 //  This is javascript of the web site
+
+function main() {
+    $('.carousel').carousel({
+        interval: 3000
+    });
+ }
